2da31679b2567e975e277bd1aeae9f85cc4e1cd5
FCGit19 <canofred@gmail.com>
2020-04-23 13:10:02 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species     
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    # @@all << self #this one is not recommended
+    Owner.all << self 
+    # self.save
+  end 
+
+
+  def say_species
+    "I am a #{self.species}."
+  end 
+  
+  def self.all
+    @@all 
+  end 
 end
+
+def self.count
+  self.all.count
+end 
+
+def self.reset_all 
+  self.call.clear
+end 
+
+def buy_cat(cat_name)
+  Cat.new(cat_name, self)
+end 
